b42933108fb5ade47d596da4db33bdf821c21103
andreas <andschar@gmail.com>
2021-03-04 18:50:46 +0100

    update

--- html2pdf.R
+++ html2pdf.R
@@ -1,6 +1,10 @@
 
-pagedown::chrome_print('GIT/github_intro.html')
+require(pagedown)
 
-pagedown::chrome_print('SQL/sql_intro.html')
+chrome_print('GIT/github_intro.html')
 
-pagedown::chrome_print('SQL/sql_intro_exercise1.html')
+chrome_print('SQL/sql_intro.html')
+
+chrome_print('SQL/sql_intro_exercise1.html')
+
+chrome_print('GAM/gam_intro.html')
