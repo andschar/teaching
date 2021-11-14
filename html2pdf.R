c8ab5d69830e8caf45a18525609f566e541bde0e
andreas <andschar@gmail.com>
2021-11-14 20:42:27 +0100

    pdfs

--- html2pdf.R
+++ html2pdf.R
@@ -3,8 +3,8 @@ require(pagedown)
 
 chrome_print('GIT/github_intro.html')
 
-chrome_print('SQL/sql_intro.html')
+chrome_print('SQL/PostgreSQL-intro.html')
 
-chrome_print('SQL/sql_intro_exercise1.html')
+chrome_print('PostGIS/PostGIS-intro.html')
 
 chrome_print('GAM/gam_intro.html')
