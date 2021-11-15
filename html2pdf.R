80f0f67e5585848f64221863c91a51814e2818f9
andreas <andschar@gmail.com>
2021-11-15 10:52:01 +0100

    update

--- html2pdf.R
+++ html2pdf.R
@@ -1,10 +1,8 @@
 
-require(pagedown)
+pagedown::chrome_print('GIT/github_intro.html')
 
-chrome_print('GIT/github_intro.html')
+pagedown::chrome_print('SQL/PostgreSQL-intro.html')
 
-chrome_print('SQL/PostgreSQL-intro.html')
+pagedown::chrome_print('PostGIS/PostGIS-intro.html')
 
-chrome_print('PostGIS/PostGIS-intro.html')
-
-chrome_print('GAM/gam_intro.html')
+pagedown::chrome_print('GAM/gam_intro.html')
